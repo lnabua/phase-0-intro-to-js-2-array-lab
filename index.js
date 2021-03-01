7e2c4bbf00962392c66188eceadb103f305160c6
lnabua <nabua.lorraine@gmail.com>
2021-02-28 17:14:29 -0800

    commit tree ff88547c7eb9770bbcc82f210dcc9894eae74933

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat () {
+    cats.push('Ralph');
+    return cats;
+}
+
+function destructivelyPrependCat () {
+    cats.unshift('Bob');
+    return cats;
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+    return cats;
+}
+
+function appendCat () {
+    const catsOne = [...cats, 'Broom'];
+    return catsOne;
+}
+
+function prependCat () {
+    const catsTwo = ['Arnold', ...cats];
+    return catsTwo;
+}
+
+function removeLastCat () {
+    const catsThree = cats.slice(0, -1);
+    return catsThree;
+}
+
+function removeFirstCat () {
+    const catsFour = cats.slice(1);
+    return catsFour;
+}
